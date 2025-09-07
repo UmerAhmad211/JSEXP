14f3ffd063ed7793724f3148708a9a38c08bdb38
UmerAhmad211 <umerahmed824@gmail.com>
2025-09-08 00:31:19 +0500

    Setup jsexp driver

--- src/main.ml
+++ src/main.ml
@@ -1,31 +1,40 @@
-open Sexplib.Sexp
+open Sexp_gen
+open Util
 
-(*
-    opam install sexplib yojson
-    ocamlfind ocamlopt -o main -linkpkg -package sexplib -package yojson main.ml
- *)
+let usage_msg = "jsexp <in_file_name> -o <out_file_name>"
+let in_file_name = ref ""
+let out_file_name = ref ""
+let speclist = [ ("-o", Arg.Set_string out_file_name, "Set output file name") ]
 
-let rec json_to_sexp (json : Yojson.Basic.t) : Sexplib.Sexp.t =
-  match json with
-  | `Null -> Atom "null"
-  | `Bool b -> Atom (string_of_bool b)
-  | `Int i -> Atom (string_of_int i)
-  | `Float f -> Atom (string_of_float f)
-  | `String s -> Atom s
-  | `List lst -> List (List.map json_to_sexp lst)
-  | `Assoc kvs ->
-      List (List.map (fun (k, v) -> List [ Atom k; json_to_sexp v ]) kvs)
+let anon_fun filename =
+  if !in_file_name = "" then
+    in_file_name := filename
+  else
+    print_err_usage_fail ~usage:usage_msg "Error: Only one file expected."
 
 let () =
-  if Array.length Sys.argv <> 2 then (
-    prerr_endline "Usage: ./main <file_name>";
-    exit 1);
-  let filename = Sys.argv.(1) in
-  try
-    let json = Yojson.Basic.from_file filename in
-    let sexp = json_to_sexp json in
-    Sexplib.Sexp.output_hum stdout sexp;
-    print_newline ()
-  with
-  | Sys_error msg -> Printf.eprintf "File error: %s\n" msg
-  | Yojson.Json_error msg -> Printf.eprintf "JSON parse error: %s\n" msg
+  Arg.parse speclist anon_fun usage_msg;
+  if !in_file_name = "" && check_filename_exe !in_file_name then
+    print_err_usage_fail ~usage:usage_msg "Error: Input file error.";
+
+  let ic = open_in !in_file_name in
+  let lexbuf = Lexing.from_channel ic in
+
+  let json =
+    try Parser.program Lexer.read_token lexbuf
+    with Parser.Error ->
+      pprint_error !in_file_name lexbuf;
+      exit 1
+  in
+  close_in ic;
+
+  let sexp = sexp_of_json json in
+  let out =
+    if !out_file_name = "" then
+      stdout
+    else
+      open_out !out_file_name
+  in
+  output_string out (string_of_sexp sexp ^ "\n");
+  if out != stdout then
+    close_out out
